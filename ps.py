714e8ad7081f4ee8df55443acd0c33ba95628bcc
Alex Musayev <alex.musayev@gmail.com>
2012-07-03 17:39:24 +0400

    process_files() function structure flattened because 'Flat is better than nested'.

--- ps.py
+++ ps.py
@@ -29,6 +29,7 @@ __url__ = "http://github.com/dreikanter/public-static"
 
 script_name = os.path.splitext(os.path.basename(__file__))[0]
 log = logging.getLogger(__name__)
+conf = {}
 
 DEFAULT_CONF = "%s.ini" % script_name
 DEFAULT_LOG = "%s.log" % script_name
@@ -65,6 +66,9 @@ TEMPLATE_FILE_NAME = "%s.mustache.html"
 LOG_CONSOLE_FORMAT = ('%(asctime)s %(levelname)s: %(message)s', '%H:%M:%S')
 LOG_FILE_FORMAT = ('%(asctime)s %(levelname)s: %(message)s', '%Y/%m/%d %H:%M:%S')
 TIME_FORMAT = "%Y/%m/%d %H:%M:%S"
+RE_FLAGS = re.I | re.M | re.U
+PARAM_PATTERN = re.compile(r"^\s*([\w\d_-]+)\s*[:=]{1}(.*)", RE_FLAGS)
+MD = markdown.Markdown(extensions=['nl2br', 'grid'])
 
 
 # Initialization =============================================================
@@ -73,7 +77,7 @@ def init(conf_file, section, log_file, verbose=False):
     """Gets the configuration values or termanates execution in case of errors"""
 
     def get_params(conf_file, section):
-        print("Using configuration from: %s (section: %s)" % (conf_file, section if section else "default"))
+        print("Configuration: %s [%s]" % (conf_file, section or "default"))
 
         parser = RawConfigParser()
         with codecs.open(conf_file, mode='r', encoding='utf8') as f:
@@ -124,6 +128,8 @@ def init(conf_file, section, log_file, verbose=False):
         print("Error initializing loggign: " + str(e))
         exit(1)
 
+    global conf
+
     try:
         conf = get_params(conf_file, section)
 
@@ -141,7 +147,7 @@ def init(conf_file, section, log_file, verbose=False):
         conf['minify_js_cmd'] = conf.get('minify_js_cmd', DEFAULT_MINIFY_JS_CMD).strip()
         if conf['minify_js'] and not conf['minify_js_cmd']:
             log.warn("JS minification enabled but [minify_js_cmd] is not defined by configuration.")
-        
+
         conf['minify_css'] = get_bool(conf.get('minify_css', 'y'))
         conf['minify_css_cmd'] = conf.get('minify_css_cmd', DEFAULT_MINIFY_CSS_CMD).strip()
         if conf['minify_css'] and not conf['minify_css_cmd']:
@@ -153,8 +159,6 @@ def init(conf_file, section, log_file, verbose=False):
 
         for param in conf:
             log.debug("%s = [%s]" % (param, conf[param]))
-
-        return conf
 
     except Exception as e:
         log.exception("Error reading configuration")
@@ -208,106 +212,112 @@ def drop_build_dir(build_path, create_new=False):
 
 # Website building ============================================================
 
-def process_files(conf):
+def get_md_h1(text):
+    """Extracts the first h1-header from markdown text."""
+    matches = re.search(r"^\s*#\s*(.*)\s*", text, RE_FLAGS)
+    return matches.group(1) if matches else ""
+
+
+def purify_time(page, time_parm, default):
+    """Returns time value from page dict. If there is no
+    specified value, default will be returned."""
+    if time_parm in page:
+        page[time_parm] = time.strptime(page[time_parm], TIME_FORMAT)
+    else:
+        page[time_parm] = datetime.fromtimestamp(default)
+
+
+def read_page_source(source_file):
+    """Reads a page file to dictionary.
+
+    TODO: Describe page format here."""
+    try:
+        page = {}
+        with codecs.open(source_file, mode='r', encoding='utf8') as f:
+            # Extract page metadata if there are some header lines
+            lines = f.readlines()
+            for num, line in enumerate(lines):
+                match = PARAM_PATTERN.match(line)
+                if match:
+                    page[match.group(1)] = match.group(2).strip()
+                else:
+                    page['content'] = ''.join(lines[num:])
+                    break
+
+        page['title'] = page.get('title', get_md_h1(page['content'])).strip()
+        page['template'] = page.get('template', DEFAULT_TEMPLATE).strip()
+        page['author'] = page.get('author', conf['default_author']).strip()
+        page['content'] = MD.convert(page.get('content', '').strip())
+
+        # Take date/time from file system if not explicitly defined
+        purify_time(page, 'ctime', os.path.getctime(source_file))
+        purify_time(page, 'mtime', os.path.getmtime(source_file))
+
+        return page
+
+    except Exception as e:
+        log.exception("Page source parsing error [%s]" % source_file)
+        return {}
+
+
+def process_dir(message, source_root):
+    log.info("Processing %s from [%s]..." % (message, source_root))
+
+    for cur_dir, dirs, files in os.walk(source_root):
+        rel_path = cur_dir[len(source_root):].strip("\\/")
+        dest_path = os.path.join(conf['build_path'], rel_path)
+        ensure_dir_exists(dest_path)
+
+        for file_name in files:
+            process_file(source_root, os.path.join(cur_dir, file_name))
+
+
+def process_file(source_root, source_file):
+    """Process single file."""
+    rel_source = os.path.relpath(source_file, source_root)
+    base, ext = os.path.splitext(rel_source)
+    rel_dest = base + ('.html' if ext == '.md' else ext)
+    dest_file = os.path.join(conf['build_path'], rel_dest)
+
+    if ext == '.md':
+        log.info(" * Building page: %s => %s" % (rel_source, rel_dest))
+        build_page(source_file, dest_file, conf['templates_path'])
+
+    elif ext == '.css' and conf['minify_css'] and conf['minify_css_cmd']:
+        log.info(' * Minifying CSS: ' + rel_source)
+        execute(conf['minify_css_cmd'].format(source=source_file, dest=dest_file))
+
+    elif ext == '.js' and conf['minify_js'] and conf['minify_js_cmd']:
+        log.info(' * Minifying JS: ' + rel_source)
+        execute(conf['minify_js_cmd'].format(source=source_file, dest=dest_file))
+
+    else:
+        log.info(' * Copying: ' + rel_source)
+        shutil.copyfile(source_file, dest_file)
+
+
+def get_template(tpl_name, templates_path):
+    file_name = os.path.join(templates_path, TEMPLATE_FILE_NAME % tpl_name)
+    if os.path.exists(file_name):
+        with codecs.open(file_name, mode='r', encoding='utf8') as f:
+            return f.read()
+
+    raise Exception("Error reading template: %s (%s)" % (tpl_name, file_name))
+
+
+def build_page(source_file, dest_file, templates_path):
+    """Builds a page from markdown source amd mustache template."""
+    try:
+        page = read_page_source(source_file)
+        with codecs.open(dest_file, mode='w', encoding='utf8') as f:
+            f.write(pystache.render(get_template(page['template'], templates_path), page))
+
+    except Exception as e:
+        log.exception("Content processing error")
+
+
+def process_files():
     """Walk through source files and process one by one."""
-
-    def process_dir(message, source_root):
-        log.info("Processing %s from [%s]..." % (message, source_root))
-
-        for cur_dir, dirs, files in os.walk(source_root):
-            dest_path = os.path.join(conf['build_path'], cur_dir[len(source_root):].strip("\\/"))
-            ensure_dir_exists(dest_path)
-
-            for file_name in files:
-                process_file(source_root, os.path.join(cur_dir, file_name))
-
-    def process_file(source_root, source_file):
-        """Process single file."""
-        rel_source_file = os.path.relpath(source_file, source_root)
-        base, ext = os.path.splitext(rel_source_file)
-        rel_dest_file = base + (".html" if ext == ".md" else ext)
-        dest_file = os.path.join(conf['build_path'], rel_dest_file)
-
-        if ext == ".md":
-            log.info(" * Building page: %s => %s" % (rel_source_file, rel_dest_file))
-            build_page(source_file, dest_file, conf['templates_path'])
-
-        elif ext == ".css" and conf['minify_css'] and conf['minify_css_cmd']:
-            log.info(" * Minifying CSS: " + rel_source_file)
-            execute(conf['minify_css_cmd'].format(source=source_file, dest=dest_file))
-
-        elif ext == ".js" and conf['minify_js'] and conf['minify_js_cmd']:
-            log.info(" * Minifying JS: " + rel_source_file)
-            execute(conf['minify_js_cmd'].format(source=source_file, dest=dest_file))
-
-        else:
-            log.info(" * Copying: " + rel_source_file)
-            shutil.copyfile(source_file, dest_file)
-
-    def build_page(source_file, dest_file, templates_path):
-        """Builds a page from markdown source amd mustache template."""
-
-        def get_md_h1(text):
-            """Extracts the first h1-header from markdown text."""
-            matches = re.search(r"^\s*#\s*(.*)\s*", text, re.I|re.M|re.U)
-            return matches.group(1) if matches else ""
-
-        def purify_time(page, time_parm, default):
-            if time_parm in page:
-                page[time_parm] = time.strptime(page[time_parm], TIME_FORMAT)
-            else:
-                page[time_parm] = datetime.fromtimestamp(default)
-
-        def read_page_source(source_file):
-            """Reads a page file to dictionary."""
-
-            try:
-                page = {}
-                with codecs.open(source_file, mode='r', encoding='utf8') as f:
-                    # Extract page metadata if there are some header lines
-                    lines = f.readlines()
-                    param_pattern = re.compile(r"^\s*([\w\d_-]+)\s*[:=]{1}(.*)", re.I|re.M|re.U)
-                    # section_pattern = re.compile(r"^$", re.I|re.M|re.U)
-
-                    for i in range(0, len(lines)):
-                        match = param_pattern.match(lines[i])
-                        if match:
-                            page[match.group(1)] = match.group(2).strip()
-                        else:
-                            page['content'] = ''.join(lines[i:])
-                            break
-
-                page['title'] = page.get('title', get_md_h1(page['content'])).strip()
-                page['template'] = page.get('template', DEFAULT_TEMPLATE).strip()
-                page['author'] = page.get('author', conf['default_author']).strip()
-                page['content'] = markdown.markdown(page.get('content', '').strip())
-
-                # Take date/time from file system if not explicitly defined
-                purify_time(page, 'ctime', os.path.getctime(source_file))
-                purify_time(page, 'mtime', os.path.getmtime(source_file))
-
-                return page
-
-            except Exception as e:
-                log.exception("Page source parsing error [%s]" % source_file)
-                return {}
-
-        def get_template(tpl_name, templates_path):
-            file_name = os.path.join(templates_path, TEMPLATE_FILE_NAME % tpl_name)
-            if os.path.exists(file_name):
-                with codecs.open(file_name, mode='r', encoding='utf8') as f:
-                    return f.read()
-
-            raise Exception("Error reading template: %s (%s)" % (tpl_name, file_name))
-
-        try:
-            page = read_page_source(source_file)
-            with codecs.open(dest_file, mode='w', encoding='utf8') as f:
-                f.write(pystache.render(get_template(page['template'], templates_path), page))
-
-        except Exception as e:
-            log.exception("Content processing error")
-
     process_dir("static files", conf['static_path'])
     process_dir("pages", conf['pages_path'])
 
@@ -317,12 +327,12 @@ def process_files(conf):
 @baker.command(shortopts=COMMON_SHORTOPS, params=COMMON_PARAMS, default=True)
 def build(config=DEFAULT_CONF, section=None, logfile=DEFAULT_LOG, verbose=False):
     """Generate web content"""
-    conf = init(config, section, logfile, verbose)
+    init(config, section, logfile, verbose)
 
     try:
         drop_build_dir(conf['build_path'])
         log.info("Building path: [%s]" % conf['build_path'])
-        process_files(conf)
+        process_files()
         log.info("Done")
 
     except Exception as e:
@@ -333,7 +343,7 @@ def build(config=DEFAULT_CONF, section=None, logfile=DEFAULT_LOG, verbose=False)
                params=joind(COMMON_PARAMS, {"browse": "Open in default browser", "port": "Port for local HTTP server"}))
 def preview(config=DEFAULT_CONF, section=None, logfile=DEFAULT_LOG, verbose=False, browse=False, port=DEFAULT_PORT):
     """Run local web server to preview generated web site"""
-    conf = init(config, section, logfile, verbose)
+    init(config, section, logfile, verbose)
 
     check_build_is_done(conf['build_path'])
     prev_cwd = os.getcwd()
@@ -363,7 +373,7 @@ def preview(config=DEFAULT_CONF, section=None, logfile=DEFAULT_LOG, verbose=Fals
 @baker.command(shortopts=COMMON_SHORTOPS, params=COMMON_PARAMS)
 def publish(config=DEFAULT_CONF, section=None, logfile=DEFAULT_LOG, verbose=False):
     """Synchronize remote web server with generated content."""
-    conf = init(config, section, logfile, verbose)
+    init(config, section, logfile, verbose)
     check_build_is_done(conf['build_path'])
 
     if conf['publish_cmd']:
@@ -382,7 +392,7 @@ def publish(config=DEFAULT_CONF, section=None, logfile=DEFAULT_LOG, verbose=Fals
 @baker.command(shortopts=COMMON_SHORTOPS, params=COMMON_PARAMS)
 def clean(config=DEFAULT_CONF, section=None, logfile=DEFAULT_LOG, verbose=False):
     """Delete all generated web content"""
-    conf = init(config, section, logfile, verbose)
+    init(config, section, logfile, verbose)
 
     try:
         log.info("Cleaning output...")
