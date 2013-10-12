64d7415ac39033e419ca40f168e43b6d1945251e
Alex Musayev <alex.musayev@gmail.com>
2013-10-12 16:30:23 +0400

    Setup script updated.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def get_desc(file_name):
     try:
         with sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE) as process:
             return process.stdout.read().decode('utf-8')
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         print('pandoc not installed, using readme contents as is')
         with codecs.open(file_name, mode='r', encoding='utf-8') as f:
             return f.read()
@@ -51,15 +51,19 @@ setup(
         'pyyaml',
         'yuicompressor',
     ],
-    entry_points={'console_scripts': ['pub = publicstatic:main']},
+    entry_points=dict(console_scripts=['pub=publicstatic:main']),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-       'Development Status :: 5 - Production/Stable',
-       'Intended Audience :: Developers',
-       'License :: OSI Approved :: MIT License',
-       'Programming Language :: Python',
-       'Programming Language :: Python :: 2.7',
-       'Programming Language :: Python :: 3.3',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
     ],
 )
