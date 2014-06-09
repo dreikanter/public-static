46d8952e93b46e8efa2181d4fdb774a2bd9b27b0
Alex Musayev <alex.musayev@gmail.com>
2014-06-09 18:18:39 +0400

    Markdown data extension moved to separate file.

--- publicstatic/markdown.py
+++ publicstatic/markdown.py
@@ -4,35 +4,7 @@ import csv
 import markdown
 import re
 from publicstatic import templates
-
-
-PREFIX = '--- data:'
-
-
-class DataPreprocessor(markdown.preprocessors.Preprocessor):
-    def run(self, lines):
-        new_lines = []
-        for line in lines:
-            if line.startswith(PREFIX):
-                data_file, template = self._parse_args(line)
-                new_lines.append(templates.render_data(data_file, template))
-            else:
-                new_lines.append(line)
-        return new_lines
-
-    def _parse_args(self, line):
-        try:
-            reader = csv.reader([line[len(PREFIX):]], skipinitialspace=True)
-            for row in reader:
-                data_file, template = row[0], row[1]
-            return data_file, template
-        except:
-            return None, None
-
-
-class DataExtension(markdown.Extension):
-    def extendMarkdown(self, md, md_globals):
-        md.preprocessors.add('data', DataPreprocessor(md), '_begin')
+from publicstatic import data
 
 
 EXTENSIONS = [
@@ -42,7 +14,7 @@ EXTENSIONS = [
     'grid',
     'nl2br',
     'smarty',
-    DataExtension(),
+    data.DataExtension(),
 ]
 
 
